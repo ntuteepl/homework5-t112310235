3ee3fcf23b83981d4e2aaa297e63ce51047ae2bd
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-31 15:22:25 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -7,7 +7,10 @@ float ht=30*H+0.5*M;
 float mt=6*M;
 double t;
     if(ht>360)
+    {
         ht=ht-360;
+    }
+
     t=ht-mt;
 
     if(t>0&&t<=180)
@@ -21,15 +24,15 @@ double t;
        printf("%.3f\n", t);
 
     }
-    if(t<0&&t<=180)
+    if(t<0&&t>=((-1)*180))
     {
         t=t*(-1);
          printf("%.3f\n", t);
     }
-    if(t<0&&t>180)
+    if(t<0&&t<((-1)*180))
     {
-        t=t+180;
-        t=t*(-1);
+        t=t+360;
+
        printf("%.3f\n", t);
 
     }
