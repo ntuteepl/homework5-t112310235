cc4507965ceb68a8a0331ee65b2d07fd39d38b4c
t112310235 <146065142+t112310235@users.noreply.github.com>
2023-10-31 14:58:22 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,37 @@
 
+#include <stdio.h>
+main()
+{
+int H,M;
+scanf("%d %d",&H,&M);
+float ht=30*H+0.5*M;
+float mt=6*M;
+double t;
+    t=ht-mt;
+    if(t>0&&t<=180)
+    {
+       printf("%.3f\n", t);
+
+    }
+    if(t>0&&t>180)
+    {
+        t=t-180;
+       printf("%.3f\n", t);
+
+    }
+    if(t<0&&t<=180)
+    {
+        t=t*(-1);
+         printf("%.3f\n", t);
+    }
+    if(t<0&&t>180)
+    {
+        t=t+180;
+        t=t*(-1);
+       printf("%.3f\n", t);
+
+    }
+
+
+
+}
